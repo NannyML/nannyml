65e18bf76831dc2b5a45255ba9bdadfd50c00e7f
Niels Nuyttens <niels@nannyml.com>
2022-02-18 13:40:50 +0100

    Added docs for ranking Added support for exclude_non_drifting on AlertCountRanking

--- nannyml/drift/ranking.py
+++ nannyml/drift/ranking.py
@@ -12,25 +12,61 @@ from nannyml.exceptions import InvalidArgumentsException
 
 
 class Ranking(abc.ABC):
-    def rank(self, drift_calculation_result: pd.DataFrame, exclude_non_drifting: bool = True) -> pd.DataFrame:
+    """Used to rank drifting features according to impact."""
+
+    def rank(self, drift_calculation_result: pd.DataFrame, exclude_non_drifting: bool = False) -> pd.DataFrame:
+        """Ranks the features within a drift calculation according to impact.
+
+        Parameters
+        ----------
+        drift_calculation_result : pd.DataFrame
+            The drift calculation results.
+        exclude_non_drifting : bool
+            Omits non-drifting features from the ranking if True.
+
+        Returns
+        -------
+        feature_ranking: pd.DataFrame
+            A DataFrame containing at least a feature name and a rank per row.
+
+        """
         raise NotImplementedError
 
 
 class AlertCountRanking(Ranking):
+    """Ranks drifting features by the number of 'alerts' they've caused."""
 
-    _ALERT_COLUMN_SUFFIX = '_alert'
+    ALERT_COLUMN_SUFFIX = '_alert'
 
-    def rank(self, drift_calculation_result: pd.DataFrame, exclude_non_drifting: bool = True) -> pd.DataFrame:
+    def rank(self, drift_calculation_result: pd.DataFrame, exclude_non_drifting: bool = False) -> pd.DataFrame:
+        """Compares the number of alerts for each feature and uses that for ranking.
 
+        Parameters
+        ----------
+        drift_calculation_result : pd.DataFrame
+            The drift calculation results. Requires alert columns to be present. These are recognized and parsed
+            using the ALERT_COLUMN_SUFFIX pattern, currently equal to ``'_alert'``.
+        exclude_non_drifting : bool
+            Omits features without alerts from the ranking results.
+
+        Returns
+        -------
+        feature_ranking: pd.DataFrame
+            A DataFrame containing the feature names and their ranks (the highest rank starts at 1,
+            second-highest rank is 2, etc.)
+
+        """
         if drift_calculation_result.empty:
             raise InvalidArgumentsException('drift results contain no data to use for ranking')
 
         alert_column_names = [
-            column_name for column_name in drift_calculation_result.columns if self._ALERT_COLUMN_SUFFIX in column_name
+            column_name for column_name in drift_calculation_result.columns if self.ALERT_COLUMN_SUFFIX in column_name
         ]
         ranking = pd.DataFrame(drift_calculation_result[alert_column_names].sum()).reset_index()
         ranking.columns = ['feature', 'number_of_alerts']
-        ranking['feature'] = ranking['feature'].str.replace(self._ALERT_COLUMN_SUFFIX, '')
+        ranking['feature'] = ranking['feature'].str.replace(self.ALERT_COLUMN_SUFFIX, '')
         ranking = ranking.sort_values('number_of_alerts', ascending=False, ignore_index=True)
         ranking['rank'] = ranking.index + 1
+        if exclude_non_drifting:
+            ranking = ranking.loc[ranking['number_of_alerts'] != 0, :]
         return ranking
--- tests/test_ranking.py
+++ tests/test_ranking.py
@@ -1,6 +1,9 @@
 #  Author:   Niels Nuyttens  <niels@nannyml.com>
 #
 #  License: Apache Software License 2.0
+
+"""Unit tests for drift ranking."""
+
 import pandas as pd
 import pytest
 
@@ -9,7 +12,7 @@ from nannyml.exceptions import InvalidArgumentsException
 
 
 @pytest.fixture
-def sample_drift_result() -> pd.DataFrame:
+def sample_drift_result() -> pd.DataFrame:  # noqa: D103
     return pd.DataFrame(
         {
             'f1_alert': [0, 0, 0, 0, 1, 1],
@@ -28,16 +31,25 @@ def test_alert_count_ranking_raises_invalid_arguments_exception_when_drift_resul
         ranking.rank(pd.DataFrame(columns=['f1', 'f1_alert']))
 
 
-def test_alert_count_ranking_contains_rank_column(sample_drift_result):
+def test_alert_count_ranking_contains_rank_column(sample_drift_result):  # noqa: D103
     ranking = AlertCountRanking()
     sut = ranking.rank(sample_drift_result)
     assert 'rank' in sut.columns
 
 
-def test_alert_count_ranks_by_sum_of_alerts_per_feature(sample_drift_result):
+def test_alert_count_ranks_by_sum_of_alerts_per_feature(sample_drift_result):  # noqa: D103
     ranking = AlertCountRanking()
     sut = ranking.rank(sample_drift_result)
     assert sut.loc[sut['rank'] == 1, 'feature'].values[0] == 'f2'
     assert sut.loc[sut['rank'] == 2, 'feature'].values[0] == 'f1'
     assert sut.loc[sut['rank'] == 3, 'feature'].values[0] == 'f3'
     assert sut.loc[sut['rank'] == 4, 'feature'].values[0] == 'f4'
+
+
+def test_alert_count_ranking_should_exclude_zero_alert_features_when_exclude_option_set(  # noqa: D103
+    sample_drift_result,
+):
+    ranking = AlertCountRanking()
+    sut = ranking.rank(sample_drift_result, exclude_non_drifting=True)
+    assert len(sut) == 3
+    assert len(sut[sut['feature'] == 'f4']) == 0
