52e9daaf3659948fd3e03ce119c885747acda33f
Niels Nuyttens <niels@nannyml.com>
2022-02-24 10:42:09 +0100

    Include CME and BasePerformanceEstimator in exported modules

--- nannyml/__init__.py
+++ nannyml/__init__.py
@@ -50,6 +50,7 @@ from .drift import (
 )
 from .exceptions import ChunkerException, InvalidArgumentsException, MissingMetadataException
 from .metadata import Feature, FeatureType, ModelMetadata, extract_metadata
+from .performance_estimation import CME, BasePerformanceEstimator
 from .plots import DriftPlots
 
 logger = logging.getLogger(__name__)
--- nannyml/performance_estimation/__init__.py
+++ nannyml/performance_estimation/__init__.py
@@ -3,3 +3,6 @@
 #  License: Apache Software License 2.0
 
 """Contains all performance estimation implementations and utilities."""
+
+from ._base import BasePerformanceEstimator
+from .confusion_matrix_estimation import CME
