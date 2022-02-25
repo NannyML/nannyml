a720a14777544a67151d4f1ff2bca061050b6a47
Niels Nuyttens <niels@nannyml.com>
2022-02-25 10:42:31 +0100

    Try and fix source code path issues

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../nannyml'))
+# sys.path.insert(0, os.path.abspath('../nannyml'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 
 import nannyml  # noqa: E402
 
