acda8b1e0c1588d329ee2ee6bbd0b21485b92c73
PieDude12 <86422883+PieDude12@users.noreply.github.com>
2022-02-28 21:25:06 +0000

    Visualisation update (#15)
    * Set joy plot overlay to 1 and created manual test folder
    
    * Updated all hover_date_label_formats to contain years. Bar plots: split hue into (1) drift dimension and (2) chunk type dimension, also implemented new design for both dimensions, updated colour palette for categories (using seaborn) and added hover data.
    
    * Joyplots: changed the line_color to represent the type of period and the fill_color both the type of period AND drift. Also changed the hover_label bgcolor.
    
    * Removerd zeroline from all plots
    
    * Made background of plots white instead of transparant
    
    * Bar_plots: only rename the consolidating category to other if there is more than one category to consolidate.
    
    * Update lock file
    
    Co-authored-by: Niels Nuyttens <niels@nannyml.com>

--- nannyml/plots/_joy_plot.py
+++ nannyml/plots/_joy_plot.py
@@ -184,10 +184,10 @@ def _create_joy_plot(
 
     layout = go.Layout(
         title=title,
-        xaxis=dict(title=x_axis_title, linecolor=colors[2], showgrid=False, mirror=True),
-        yaxis=dict(title=y_axis_title, linecolor=colors[2], showgrid=False, mirror=True, autorange="reversed"),
-        paper_bgcolor='rgba(0,0,0,0)',
-        plot_bgcolor='rgba(0,0,0,0)',
+        xaxis=dict(title=x_axis_title, linecolor=colors[2], showgrid=False, mirror=True, zeroline=False),
+        yaxis=dict(title=y_axis_title, linecolor=colors[2], showgrid=False, mirror=True, autorange="reversed", zeroline=False),
+        paper_bgcolor='rgba(255,255,255,1)',
+        plot_bgcolor='rgba(255,255,255,1)',
         legend=dict(itemclick=False, itemdoubleclick=False),
     )
 
@@ -206,7 +206,8 @@ def _create_joy_plot(
         kde_density_scaled = row['kde_density_scaled'] * joy_overlap
         kde_cdf = row['kde_cdf']
         kde_quartiles = [(q[0], q[1] * joy_overlap) for q in row['kde_quartiles_scaled']]
-        color = colors[row['hue']]
+        color = colors[int(row[chunk_type_column_name] == chunk_types[1])]
+        color_drift = colors[row['hue']]
         color_fill = colors_transparent[row['hue']]
         trace_name = hue_joy_hover_labels[row['hue']]
 
@@ -233,6 +234,7 @@ def _create_joy_plot(
                 hovertemplate=hover_template,
                 customdata=hover_data,
                 showlegend=False,
+                hoverlabel = dict(bgcolor=color_drift, font=dict(color='white'))
             )
         )
 
@@ -255,7 +257,7 @@ def _create_joy_plot(
                         x=[kde_quartile[0], kde_quartile[0]],
                         y=[y_date_position, y_date_position + kde_quartile[1] * y_date_height_scaler],
                         mode='lines',
-                        line=dict(color=color, width=1, dash='dot'),
+                        line=dict(color=color_drift, width=1, dash='dot'),
                         hoverinfo='skip',
                         showlegend=False,
                     )
@@ -277,7 +279,7 @@ def _create_joy_plot(
                     x=x,
                     y=y,
                     mode='lines',
-                    line=dict(color=colors[i], width=1),
+                    line=dict(color=colors[i] if i != -1 else 'rgba(0,0,0,0)', width=1),
                     fill='tonexty',
                     fillcolor=colors_transparent[i],
                 ),
@@ -316,9 +318,9 @@ def _joy_plot(
     chunk_hover_label='Chunk',
     hue_joy_hover_labels=None,
     quartiles_legend_label='Quartiles',
-    date_label_hover_format='%d/%b',
+    date_label_hover_format='%d/%b/%y',
     joy_hover_format='{0:.2f}',
-    joy_overlap=2,
+    joy_overlap=1,
     figure=None,
     title='Feature: distribution over time',
     x_axis_title='Feature',
