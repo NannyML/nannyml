2b160f87adc838e2a42a975bd6c264c3ece4e3f2
nnansters <94110348+nnansters@users.noreply.github.com>
2022-03-29 17:14:23 +0200

    Minimum chunk size calculations for metrics (#49)
    * implement analytical roc auc stability estimator
    
    * min chunk f1
    
    * add precision and flooring function
    
    * add recall
    
    * remove sensitivity, keep recall;
    
    * min chunk size specificity
    
    * clear sensitivity;
    
    * min chunk accuracy
    
    * add simple unittest for min chunk size
    
    * specify default min chunk size, add ceiling equal to size of reference
    
    * docstrings and refactor
    
    * Add fallback for minimum chunk calculations upon failure
    
    * Fix linting
    
    Co-authored-by: Jakub Bialek <jakub@nannyml.com>

--- nannyml/performance_calculation/metrics.py
+++ nannyml/performance_calculation/metrics.py
@@ -9,7 +9,6 @@ from typing import Dict, List, Tuple, Union
 import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, roc_auc_score
-from sklearn.preprocessing import PolynomialFeatures
 
 from nannyml import Chunk, Chunker
 from nannyml.exceptions import InvalidArgumentsException
@@ -97,7 +96,11 @@ class Metric(abc.ABC):
 
     def minimum_chunk_size(self) -> int:
         """Determines the minimum number of observations a chunk should ideally for this metric to be trustworthy."""
-        return self._minimum_chunk_size()
+        try:
+            return self._minimum_chunk_size()
+        except Exception:
+            # TODO: log failure
+            return 300
 
     def _minimum_chunk_size(self):
         raise NotImplementedError
@@ -121,59 +124,211 @@ class Metric(abc.ABC):
         )
 
 
-def _minimum_roc_auc_based_chunk_size(
+def _floor_chunk_size(calculated_min_chunk_size: float, lower_limit_on_chunk_size: int = 300) -> int:
+    return int(np.maximum(calculated_min_chunk_size, lower_limit_on_chunk_size))
+
+
+def _minimum_chunk_size_roc_auc(
     data: pd.DataFrame,
     partition_column_name: str = NML_METADATA_PARTITION_COLUMN_NAME,
     predicted_probability_column_name: str = NML_METADATA_PREDICTED_PROBABILITY_COLUMN_NAME,
     target_column_name: str = NML_METADATA_TARGET_COLUMN_NAME,
-    lower_threshold: int = 300,
+    required_std: float = 0.02,
 ) -> int:
-    def get_prediction(X):
-        # model data
-        h_coefs = [
-            0.00000000e00,
-            -3.46098897e04,
-            2.65871679e04,
-            3.46098897e04,
-            2.29602791e04,
-            -4.96886646e04,
-            -1.12777343e-10,
-            -2.29602791e04,
-            3.13775672e-10,
-            2.48718826e04,
-        ]
-        h_intercept = 1421.9522967076875
-        transformation = PolynomialFeatures(3)
-        #
+    """Estimation of minimum sample size to get required standard deviation of AUROC.
 
-        inputs = np.asarray(X)
-        transformed_inputs = transformation.fit_transform(inputs)
-        prediction = np.dot(transformed_inputs, h_coefs)[0] + h_intercept
-
-        return prediction
-
-    class_balance = np.mean(data[target_column_name])
-
-    # Clean up NaN values
+    Estimation takes advantage of Standard Error of the Mean formula and expressing AUROC as Mann-Whitney U statistic.
+    """
     y_true = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, target_column_name]
     y_pred_proba = data.loc[
         data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, predicted_probability_column_name
     ]
 
-    y_true = y_true[~y_pred_proba.isna()]
-    y_pred_proba.dropna(inplace=True)
+    y_true, y_pred_proba = np.asarray(y_true), np.asarray(y_pred_proba)
+    if np.mean(y_true) > 0.5:
+        y_true = abs(np.asarray(y_true) - 1)
+        y_pred_proba = 1 - y_pred_proba
 
-    y_pred_proba = y_pred_proba[~y_true.isna()]
-    y_true.dropna(inplace=True)
+    sorted_idx = np.argsort(y_pred_proba)
+    y_pred_proba = y_pred_proba[sorted_idx]
+    y_true = y_true[sorted_idx]
+    rank_order = np.asarray(range(len(y_pred_proba)))
+    positive_ranks = y_true * rank_order
+    indexes = np.unique(positive_ranks)[1:]
+    ser = []
 
-    auc = roc_auc_score(y_true=y_true, y_score=y_pred_proba)
+    for i, index in enumerate(indexes):
+        ser.append(index - i)
 
-    chunk_size = get_prediction([[class_balance, auc]])
-    chunk_size = np.maximum(lower_threshold, chunk_size)
-    chunk_size = np.round(chunk_size, -2)
-    minimum_chunk_size = int(chunk_size)
+    n_pos = np.sum(y_true)
+    n_neg = len(y_true) - n_pos
+    ser_divided = ser / (n_pos * n_neg)
+    ser_multi = ser_divided * n_pos
 
-    return minimum_chunk_size
+    pos_targets = y_true
+    # neg_targets = abs(y_true - 1)
+
+    n_pos_targets = np.sum(pos_targets)
+
+    fraction = n_pos_targets / len(y_true)
+    sample_size = (np.std(ser_multi)) ** 2 / ((required_std**2) * fraction)
+    sample_size = np.minimum(sample_size, len(y_true))
+    sample_size = np.round(sample_size, -2)
+
+    return _floor_chunk_size(sample_size)
+
+
+def _minimum_chunk_size_f1(
+    data: pd.DataFrame,
+    partition_column_name: str = NML_METADATA_PARTITION_COLUMN_NAME,
+    prediction_column_name: str = NML_METADATA_PREDICTION_COLUMN_NAME,
+    target_column_name: str = NML_METADATA_TARGET_COLUMN_NAME,
+    required_std: float = 0.02,
+):
+    """Estimation of minimum sample size to get required standard deviation of F1.
+
+    Estimation takes advantage of Standard Error of the Mean formula.
+    """
+    y_true = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, target_column_name]
+    y_pred = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, prediction_column_name]
+
+    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+
+    TP = np.where((y_true == y_pred) & (y_pred == 1), 1, np.nan)
+    FP = np.where((y_true != y_pred) & (y_pred == 1), 0, np.nan)
+    FN = np.where((y_true != y_pred) & (y_pred == 0), 0, np.nan)
+
+    TP = TP[~np.isnan(TP)]
+    FN = FN[~np.isnan(FN)]
+    FP = FP[~np.isnan(FP)]
+
+    tp_fp_fn = np.concatenate([TP, FN, FP])
+
+    correcting_factor = len(tp_fp_fn) / ((len(FN) + len(FP)) * 0.5 + len(TP))
+    obs_level_f1 = tp_fp_fn * correcting_factor
+    fraction_of_relevant = len(tp_fp_fn) / len(y_pred)
+    sample_size = ((np.std(obs_level_f1)) ** 2) / ((required_std**2) * fraction_of_relevant)
+    sample_size = np.minimum(sample_size, len(y_true))
+    sample_size = np.round(sample_size, -2)
+
+    return _floor_chunk_size(sample_size)
+
+
+def _minimum_chunk_size_precision(
+    data: pd.DataFrame,
+    partition_column_name: str = NML_METADATA_PARTITION_COLUMN_NAME,
+    prediction_column_name: str = NML_METADATA_PREDICTION_COLUMN_NAME,
+    target_column_name: str = NML_METADATA_TARGET_COLUMN_NAME,
+    required_std: float = 0.02,
+):
+    """Estimation of minimum sample size to get required standard deviation of Precision.
+
+    Estimation takes advantage of Standard Error of the Mean formula.
+    """
+    y_true = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, target_column_name]
+    y_pred = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, prediction_column_name]
+
+    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+
+    TP = np.where((y_true == y_pred) & (y_pred == 1), 1, np.nan)
+    FP = np.where((y_true != y_pred) & (y_pred == 1), 0, np.nan)
+
+    TP = TP[~np.isnan(TP)]
+    FP = FP[~np.isnan(FP)]
+    obs_level_precision = np.concatenate([TP, FP])
+    amount_positive_pred = np.sum(y_pred)
+    fraction_of_pos_pred = amount_positive_pred / len(y_pred)
+    sample_size = ((np.std(obs_level_precision)) ** 2) / ((required_std**2) * fraction_of_pos_pred)
+    sample_size = np.minimum(sample_size, len(y_true))
+    sample_size = np.round(sample_size, -2)
+
+    return _floor_chunk_size(sample_size)
+
+
+def _minimum_chunk_size_recall(
+    data: pd.DataFrame,
+    partition_column_name: str = NML_METADATA_PARTITION_COLUMN_NAME,
+    prediction_column_name: str = NML_METADATA_PREDICTION_COLUMN_NAME,
+    target_column_name: str = NML_METADATA_TARGET_COLUMN_NAME,
+    required_std: float = 0.02,
+):
+    """Estimation of minimum sample size to get required standard deviation of Recall.
+
+    Estimation takes advantage of Standard Error of the Mean formula.
+    """
+    y_true = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, target_column_name]
+    y_pred = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, prediction_column_name]
+
+    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+
+    TP = np.where((y_true == y_pred) & (y_pred == 1), 1, np.nan)
+    FN = np.where((y_true != y_pred) & (y_pred == 0), 0, np.nan)
+    TP = TP[~np.isnan(TP)]
+    FN = FN[~np.isnan(FN)]
+
+    obs_level_recall = np.concatenate([TP, FN])
+    fraction_of_relevant = sum(obs_level_recall) / len(y_pred)
+
+    sample_size = ((np.std(obs_level_recall)) ** 2) / ((required_std**2) * fraction_of_relevant)
+    sample_size = np.minimum(sample_size, len(y_true))
+    sample_size = np.round(sample_size, -2)
+
+    return _floor_chunk_size(sample_size)
+
+
+def _minimum_chunk_size_specificity(
+    data: pd.DataFrame,
+    partition_column_name: str = NML_METADATA_PARTITION_COLUMN_NAME,
+    prediction_column_name: str = NML_METADATA_PREDICTION_COLUMN_NAME,
+    target_column_name: str = NML_METADATA_TARGET_COLUMN_NAME,
+    required_std: float = 0.02,
+):
+    """Estimation of minimum sample size to get required standard deviation of Specificity.
+
+    Estimation takes advantage of Standard Error of the Mean formula.
+    """
+    y_true = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, target_column_name]
+    y_pred = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, prediction_column_name]
+
+    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+    TN = np.where((y_true == y_pred) & (y_pred == 0), 1, np.nan)
+    FP = np.where((y_true != y_pred) & (y_pred == 1), 0, np.nan)
+    TN = TN[~np.isnan(TN)]
+    FP = FP[~np.isnan(FP)]
+
+    obs_level_specificity = np.concatenate([TN, FP])
+    fraction_of_relevant = len(obs_level_specificity) / len(y_pred)
+    sample_size = ((np.std(obs_level_specificity)) ** 2) / ((required_std**2) * fraction_of_relevant)
+    sample_size = np.minimum(sample_size, len(y_true))
+    sample_size = np.round(sample_size, -2)
+
+    return _floor_chunk_size(sample_size)
+
+
+def _minimum_chunk_size_accuracy(
+    data: pd.DataFrame,
+    partition_column_name: str = NML_METADATA_PARTITION_COLUMN_NAME,
+    prediction_column_name: str = NML_METADATA_PREDICTION_COLUMN_NAME,
+    target_column_name: str = NML_METADATA_TARGET_COLUMN_NAME,
+    required_std: float = 0.02,
+):
+    """Estimation of minimum sample size to get required standard deviation of Accuracy.
+
+    Estimation takes advantage of Standard Error of the Mean formula.
+    """
+    y_true = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, target_column_name]
+    y_pred = data.loc[data[partition_column_name] == NML_METADATA_REFERENCE_PARTITION_NAME, prediction_column_name]
+
+    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+    y_true = np.asarray(y_true).astype(int)
+
+    y_pred = np.asarray(y_pred).astype(int)
+    correct_table = (y_true == y_pred).astype(int)
+    sample_size = (np.std(correct_table) ** 2) / (required_std**2)
+    sample_size = np.minimum(sample_size, len(y_true))
+    sample_size = np.round(sample_size, -2)
+
+    return _floor_chunk_size(sample_size)
 
 
 class AUROC(Metric):
@@ -188,7 +343,7 @@ class AUROC(Metric):
         return self._min_chunk_size
 
     def _fit(self, reference_data: pd.DataFrame):
-        self._min_chunk_size = _minimum_roc_auc_based_chunk_size(reference_data)
+        self._min_chunk_size = _minimum_chunk_size_roc_auc(reference_data)
 
     def _calculate(self, data: pd.DataFrame):
         """Redefine to handle NaNs and edge cases."""
@@ -209,12 +364,13 @@ class F1(Metric):
     def __init__(self):
         """Creates a new F1 instance."""
         super().__init__(display_name='F1')
+        self._min_chunk_size = None
 
     def _minimum_chunk_size(self) -> int:
-        return 300
+        return self._min_chunk_size
 
     def _fit(self, reference_data: pd.DataFrame):
-        pass
+        self._min_chunk_size = _minimum_chunk_size_f1(reference_data)
 
     def _calculate(self, data: pd.DataFrame):
         """Redefine to handle NaNs and edge cases."""
@@ -235,12 +391,13 @@ class Precision(Metric):
     def __init__(self):
         """Creates a new Precision instance."""
         super().__init__(display_name='precision')
+        self._min_chunk_size = None
 
     def _minimum_chunk_size(self) -> int:
-        return 300
+        return self._min_chunk_size
 
     def _fit(self, reference_data: pd.DataFrame):
-        pass
+        self._min_chunk_size = _minimum_chunk_size_precision(reference_data)
 
     def _calculate(self, data: pd.DataFrame):
         y_true = data[NML_METADATA_TARGET_COLUMN_NAME]
@@ -255,17 +412,18 @@ class Precision(Metric):
 
 
 class Recall(Metric):
-    """Recall metric."""
+    """Recall metric, also known as 'sensitivity'."""
 
     def __init__(self):
         """Creates a new Recall instance."""
         super().__init__(display_name='recall')
+        self._min_chunk_size = None
 
     def _minimum_chunk_size(self) -> int:
-        return 300
+        return self._min_chunk_size
 
     def _fit(self, reference_data: pd.DataFrame):
-        pass
+        self._min_chunk_size = _minimum_chunk_size_recall(reference_data)
 
     def _calculate(self, data: pd.DataFrame):
         y_true = data[NML_METADATA_TARGET_COLUMN_NAME]
@@ -285,12 +443,13 @@ class Specificity(Metric):
     def __init__(self):
         """Creates a new F1 instance."""
         super().__init__(display_name='specificity')
+        self._min_chunk_size = None
 
     def _minimum_chunk_size(self) -> int:
-        return 300
+        return self._min_chunk_size
 
     def _fit(self, reference_data: pd.DataFrame):
-        pass
+        self._min_chunk_size = _minimum_chunk_size_specificity(reference_data)
 
     def _calculate(self, data: pd.DataFrame):
         y_true = data[NML_METADATA_TARGET_COLUMN_NAME]
@@ -310,49 +469,19 @@ class Specificity(Metric):
             return tn / (tn + fp)
 
 
-class Sensitivity(Metric):
-    """Sensitivity metric."""
-
-    def __init__(self):
-        """Creates a new Sensitivity instance."""
-        super().__init__(display_name='sensitivity')
-
-    def _minimum_chunk_size(self) -> int:
-        return 300
-
-    def _fit(self, reference_data: pd.DataFrame):
-        pass
-
-    def _calculate(self, data: pd.DataFrame):
-        y_true = data[NML_METADATA_TARGET_COLUMN_NAME]
-        y_pred = data[NML_METADATA_PREDICTION_COLUMN_NAME]
-
-        if y_pred.isna().all():
-            raise InvalidArgumentsException(
-                f"could not calculate metric {self.display_name}: " "prediction column contains no data"
-            )
-
-        y_true, y_pred = _common_data_cleaning(y_true, y_pred)
-
-        if (y_true.nunique() <= 1) or (y_pred.nunique() <= 1):
-            return np.nan
-        else:
-            tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
-            return tp / (tp + fn)
-
-
 class Accuracy(Metric):
     """Accuracy metric."""
 
     def __init__(self):
         """Creates a new Accuracy instance."""
         super().__init__(display_name='accuracy')
+        self._min_chunk_size = None
 
     def _minimum_chunk_size(self) -> int:
-        return 300
+        return self._min_chunk_size
 
     def _fit(self, reference_data: pd.DataFrame):
-        pass
+        self._min_chunk_size = _minimum_chunk_size_accuracy(reference_data)
 
     def _calculate(self, data: pd.DataFrame):
         y_true = data[NML_METADATA_TARGET_COLUMN_NAME]
@@ -394,7 +523,6 @@ class MetricFactory:
         'f1': F1(),
         'precision': Precision(),
         'recall': Recall(),
-        'sensitivity': Sensitivity(),
         'specificity': Specificity(),
         'accuracy': Accuracy(),
     }
