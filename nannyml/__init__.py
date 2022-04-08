08c5f898c365a8969eee8158c1f9bde2663cc692
Niels Nuyttens <niels@nannyml.com>
2022-04-08 22:33:16 +0200

    Bump version: 0.2.1 → 0.3.0

--- nannyml/__init__.py
+++ nannyml/__init__.py
@@ -32,7 +32,7 @@ __email__ = 'niels@nannyml.com'
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 import logging
 
