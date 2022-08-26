d506c09b0ddd9312be99f662b52695dd84bb67ee
Niels Nuyttens <niels.nuyttens@nannyml.com>
2022-08-26 13:22:58 +0200

    Loop over metrics in DEE result plots

--- nannyml/performance_estimation/direct_error_estimation/result.py
+++ nannyml/performance_estimation/direct_error_estimation/result.py
@@ -1,9 +1,12 @@
+from typing import Union
+
 import pandas as pd
 from plotly.graph_objects import Figure
 
+from nannyml._typing import UseCase
 from nannyml.base import AbstractEstimator, AbstractEstimatorResult
 from nannyml.exceptions import InvalidArgumentsException
-from nannyml.performance_estimation.direct_error_estimation import DEFAULT_METRICS
+from nannyml.performance_estimation.direct_error_estimation.metrics import Metric, MetricFactory
 from nannyml.plots import CHUNK_KEY_COLUMN_NAME
 from nannyml.plots._step_plot import _step_plot
 
@@ -25,24 +28,28 @@ class Result(AbstractEstimatorResult):
         return "direct_error_estimator"
 
     def plot(
-        self, kind: str = 'performance', metric: str = None, plot_reference: bool = False, *args, **kwargs
+        self,
+        kind: str = 'performance',
+        metric: Union[str, Metric] = None,
+        plot_reference: bool = False,
+        *args,
+        **kwargs,
     ) -> Figure:
         if kind == 'performance':
             if metric is None:
                 raise InvalidArgumentsException(
                     "no value for 'metric' given. Please provide the name of a metric to display."
                 )
-            if metric not in DEFAULT_METRICS:
-                raise InvalidArgumentsException(
-                    f"unknown 'metric' value: '{metric}'. " f"Supported values are {DEFAULT_METRICS}."
-                )
+            if isinstance(metric, str):
+                metric = MetricFactory.create(metric, UseCase.REGRESSION, {'estimator': self.estimator})
+
             return _plot_direct_error_estimation_performance(self.data, metric, self.estimator, plot_reference)
         else:
             raise InvalidArgumentsException(f"unknown plot kind '{kind}'. " f"Please provide on of: ['performance'].")
 
 
 def _plot_direct_error_estimation_performance(
-    estimation_results: pd.DataFrame, metric: str, estimator, plot_reference: bool
+    estimation_results: pd.DataFrame, metric: Metric, estimator, plot_reference: bool
 ) -> Figure:
     estimation_results = estimation_results.copy()
 
@@ -59,10 +66,13 @@ def _plot_direct_error_estimation_performance(
 
     # TODO: hack, assembling single results column to pass to plotting, overriding alert cols
     estimation_results['plottable'] = estimation_results.apply(
-        lambda r: r[f'estimated_{metric}'] if r['period'] == 'analysis' else r[f'realized_{metric}'], axis=1
+        lambda r: r[f'estimated_{metric.column_name}']
+        if r['period'] == 'analysis'
+        else r[f'realized_{metric.column_name}'],
+        axis=1,
     )
     estimation_results['alert'] = estimation_results.apply(
-        lambda r: r[f'alert_{metric}'] if r['period'] == 'analysis' else False, axis=1
+        lambda r: r[f'alert_{metric.column_name}'] if r['period'] == 'analysis' else False, axis=1
     )
 
     # Plot estimated performance
@@ -70,21 +80,24 @@ def _plot_direct_error_estimation_performance(
         table=estimation_results,
         metric_column_name='plottable',
         chunk_column_name=CHUNK_KEY_COLUMN_NAME,
-        chunk_legend_labels=[f'Reference period (realized {metric})', f'Analysis period (estimated {metric})'],
+        chunk_legend_labels=[
+            f'Reference period (realized {metric.display_name})',
+            f'Analysis period (estimated {metric.display_name})',
+        ],
         drift_column_name='alert',
         drift_legend_label='Degraded performance',
-        hover_labels=['Chunk', f'{metric}', 'Target data'],
+        hover_labels=['Chunk', f'{metric.display_name}', 'Target data'],
         hover_marker_labels=['Reference', 'No change', 'Change'],
-        lower_threshold_column_name=f'lower_threshold_{metric}',
-        upper_threshold_column_name=f'upper_threshold_{metric}',
+        lower_threshold_column_name=f'lower_threshold_{metric.column_name}',
+        upper_threshold_column_name=f'upper_threshold_{metric.column_name}',
         threshold_legend_label='Performance threshold',
-        title=f'DEE - Estimated {metric}',
-        y_axis_title=f'{metric}',
+        title=f'DEE - Estimated {metric.display_name}',
+        y_axis_title=f'{metric.display_name}',
         v_line_separating_analysis_period=plot_period_separator,
         estimated_column_name='estimated',
-        lower_confidence_column_name=f'lower_confidence_{metric}',
-        upper_confidence_column_name=f'upper_confidence_{metric}',
-        sampling_error_column_name=f'sampling_error_{metric}',
+        lower_confidence_column_name=f'lower_confidence_{metric.column_name}',
+        upper_confidence_column_name=f'upper_confidence_{metric.column_name}',
+        sampling_error_column_name=f'sampling_error_{metric.column_name}',
     )
 
     return fig
