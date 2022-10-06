Fix realized performance calculation for regression error when no target values present
Signed-off-by: Niels Nuyttens <niels@nannyml.com>

nannyml/performance_calculation/metrics/regression.py
@@ -4,6 +4,7 @@
 from abc import ABC
 from typing import List, Optional, Tuple
 
+import numpy as np
 import pandas as pd
 from sklearn.metrics import (
     mean_absolute_error,
@@ -82,7 +83,10 @@ class MAE(RegressionMetric):
 
         y_true, y_pred = _common_data_cleaning(y_true, y_pred)
 
-        return mean_absolute_error(y_true, y_pred)
+        if (y_true.nunique() <= 1) or (y_pred.nunique() <= 1):
+            return np.nan
+        else:
+            return mean_absolute_error(y_true, y_pred)
 
     def _sampling_error(self, data: pd.DataFrame) -> float:
         return mae_sampling_error(self._sampling_error_components, data)
@@ -118,7 +122,10 @@ class MAPE(RegressionMetric):
 
         y_true, y_pred = _common_data_cleaning(y_true, y_pred)
 
-        return mean_absolute_percentage_error(y_true, y_pred)
+        if (y_true.nunique() <= 1) or (y_pred.nunique() <= 1):
+            return np.nan
+        else:
+            return mean_absolute_percentage_error(y_true, y_pred)
 
     def _sampling_error(self, data: pd.DataFrame) -> float:
         return mape_sampling_error(self._sampling_error_components, data)
@@ -154,7 +161,10 @@ class MSE(RegressionMetric):
 
         y_true, y_pred = _common_data_cleaning(y_true, y_pred)
 
-        return mean_squared_error(y_true, y_pred)
+        if (y_true.nunique() <= 1) or (y_pred.nunique() <= 1):
+            return np.nan
+        else:
+            return mean_squared_error(y_true, y_pred)
 
     def _sampling_error(self, data: pd.DataFrame) -> float:
         return mse_sampling_error(self._sampling_error_components, data)
@@ -195,7 +205,10 @@ class MSLE(RegressionMetric):
         _raise_exception_for_negative_values(y_true)
         _raise_exception_for_negative_values(y_pred)
 
-        return mean_squared_log_error(y_true, y_pred)
+        if (y_true.nunique() <= 1) or (y_pred.nunique() <= 1):
+            return np.nan
+        else:
+            return mean_squared_log_error(y_true, y_pred)
 
     def _sampling_error(self, data: pd.DataFrame) -> float:
         return msle_sampling_error(self._sampling_error_components, data)
@@ -231,7 +244,10 @@ class RMSE(RegressionMetric):
 
         y_true, y_pred = _common_data_cleaning(y_true, y_pred)
 
-        return mean_squared_error(y_true, y_pred, squared=False)
+        if (y_true.nunique() <= 1) or (y_pred.nunique() <= 1):
+            return np.nan
+        else:
+            return mean_squared_error(y_true, y_pred, squared=False)
 
     def _sampling_error(self, data: pd.DataFrame) -> float:
         return rmse_sampling_error(self._sampling_error_components, data)
@@ -272,7 +288,10 @@ class RMSLE(RegressionMetric):
         _raise_exception_for_negative_values(y_true)
         _raise_exception_for_negative_values(y_pred)
 
-        return mean_squared_log_error(y_true, y_pred, squared=False)
+        if (y_true.nunique() <= 1) or (y_pred.nunique() <= 1):
+            return np.nan
+        else:
+            return mean_squared_log_error(y_true, y_pred, squared=False)
 
     def _sampling_error(self, data: pd.DataFrame) -> float:
         return rmsle_sampling_error(self._sampling_error_components, data)