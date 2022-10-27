0ad7dcd1706295ed93c0a3422f9ebf5a49a942b5
Jakub Bialek <jakub@nannyml.com>
2022-10-27 17:32:29 +0200

    3. Add tests (and modify example notebook)

---
+++ tests/drift/test_univariate_drift_methods.py
@@ -0,0 +1,45 @@
+"""Unit tests for the UnivariateDriftCalculator methods."""
+import pandas as pd
+import numpy as np
+
+from nannyml.drift.univariate.methods import JensenShannonDistance
+
+
+def test_js_for_0_distance():
+    np.random.seed(1)
+    reference = pd.Series(np.random.choice(np.linspace(0, 2, 6), 10_000))
+    js = JensenShannonDistance()
+    js.fit(reference)
+    distance = js.calculate(reference)
+    assert distance == 0
+
+
+def test_js_for_both_continuous():
+    np.random.seed(1)
+    reference = pd.Series(np.random.normal(0, 1, 10_000))
+    analysis = pd.Series(np.random.normal(0, 1, 1000))
+    js = JensenShannonDistance()
+    js.fit(reference)
+    distance = js.calculate(analysis)
+    assert np.round(distance, 2) == 0.04
+
+
+def test_js_for_quasi_continuous():
+    np.random.seed(1)
+    reference = pd.Series(np.random.choice(np.linspace(0, 2, 6), 10_000))
+    analysis = pd.Series(np.random.choice(np.linspace(0, 2, 3), 1000))
+    js = JensenShannonDistance()
+    js.fit(reference)
+    distance = js.calculate(analysis)
+    assert np.round(distance, 2) == 0.61
+
+
+def test_js_for_categorical():
+    np.random.seed(1)
+    reference = pd.Series(np.random.choice(['a', 'b', 'c', 'd'], 10_000))
+    analysis = pd.Series(np.random.choice(['a', 'b', 'c', 'e'], 1000))
+    js = JensenShannonDistance()
+    js.fit(reference)
+    distance = js.calculate(analysis)
+    assert np.round(distance, 2) == 0.42
+
