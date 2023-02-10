072850959e7d267d94597d825b627804d3de55a4
--unset <--unset>
2023-02-10 18:19:45 +0530

    add docstrings for method_estimation

--- nannyml/drift/univariate/calculator.py
+++ nannyml/drift/univariate/calculator.py
@@ -32,7 +32,7 @@ class UnivariateDriftCalculator(AbstractCalculator):
         chunk_number: Optional[int] = None,
         chunk_period: Optional[str] = None,
         chunker: Optional[Chunker] = None,
-        method_estimation: Optional[dict[str, any]] = None,
+        method_estimation: Optional[dict[str, Any]] = None,
     ):
         """Creates a new UnivariateDriftCalculator instance.
 
@@ -58,9 +58,20 @@ class UnivariateDriftCalculator(AbstractCalculator):
             Only one of `chunk_size`, `chunk_number` or `chunk_period` should be given.
         chunker : Chunker
             The `Chunker` used to split the data sets into a lists of chunks.
-        calculation_method : str
-            The `calculation_method` used to measure the distance/statistic based on whether the exact or
-            the estimated reference data is used.
+        method_estimtion : dict, default={'kolmogorov_smirnov':{'calculation_method':{'auto', 'exact', 'estimated}, 'n_bins':10_000}, 'wasserstein':{'calculation_method':{'auto', 'exact', 'estimated}, 'n_bins':10000}}
+            
+            A dictionary which allows users to specify whether they want drift calculated on
+            the exact reference data or an estimated distribution of the reference data obtained 
+            using binning techniques. Applicable only to Kolmogorov-Smirnov and Wasserstein.
+
+            `calculation_method` : Specify whether the entire or the binned reference data will be stored. The default value is `auto`.
+
+                - `auto` : Use `exact` for reference data smaller than 10000 rows, `estimated` for larger.
+                - `exact` : Store the whole reference data. When calculating on chunk `scipy.stats.ks_2samp(reference, chunk,  method = `exact` )` is called and whole reference and chunk vectors are passed.
+                - `estimated` : Store reference data binned into `n_bins` (default=10000). The D-statistic will be calculated based on binned eCDF. Bins are quantile-based for Kolmogorov-Smirnov and equal-width based for Wasserstein. Notice that for the reference data of 10000 rows the resulting D-statistic for exact and estimated methods should be the same. The pvalue in that method is calculated using asymptotic distribution of test statistic (as it is in the `scipy.stats.ks_2samp` with method = `asymp` ).
+
+            `n_bins` : Number of bins used to bin data when calculation_method = `estimated` . The default value is 10000. The larger the value the more precise the calculation (closer to  calculation_method = `exact` ) but more data will be stored in the fitted calculator.
+
 
         Examples
         --------
@@ -104,7 +115,7 @@ class UnivariateDriftCalculator(AbstractCalculator):
         assert isinstance(categorical_methods, list)
         self.categorical_method_names: List[str] = categorical_methods
 
-        self.method_estimation: Optional[Dict[str, any]] = method_estimation
+        self.method_estimation: Optional[Dict[str, Any]] = method_estimation
 
         # set to default values within the method function in methods.py
 
