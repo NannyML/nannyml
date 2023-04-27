b238702a05288ead2392a079b4313d522949396f
Nikolaos Perrakis <nikos@nannyml.com>
2023-04-27 13:07:30 +0300

    quick draft for simple stats sum functionality

---
+++ nannyml/stats/__init__.py
@@ -0,0 +1,7 @@
+#  Author:   Nikolaos Perrakis  <nikos@nannyml.com>
+#
+#  License: Apache Software License 2.0
+
+"""Package containing the Simple Statistics implementation."""
+
+from .sum import SimpleStatsSumCalculator
