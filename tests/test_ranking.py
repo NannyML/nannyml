2680ad76ee13e6f1970f627027444a48efb9f868
Nikolaos Perrakis <nikos@nannyml.com>
2023-05-04 00:33:33 +0300

    expand rankers to work with dq and ss results

--- tests/test_ranking.py
+++ tests/test_ranking.py
@@ -12,9 +12,17 @@ from nannyml.datasets import (
     load_synthetic_binary_classification_dataset,
     load_synthetic_car_price_dataset,
     load_synthetic_multiclass_classification_dataset,
+    load_synthetic_car_loan_data_quality_dataset
 )
 from nannyml.drift.ranker import AlertCountRanker, CorrelationRanker
 from nannyml.drift.univariate import Result as UnivariateResults
+from nannyml.data_quality.missing.result import Result as MissingValueResults
+from nannyml.data_quality.missing.calculator import MissingValuesCalculator
+# from nannyml.data_quality.unseen.result import Result as UnseenValuesResult
+# from nannyml.stats.avg.result import Result as StatsAvgResult
+# from nannyml.stats.count import Result as StatsCountResult
+# from nannyml.stats.std import Result as StatsStdResults
+# from nannyml.stats.sum import Result as StatsSumResult
 from nannyml.drift.univariate import UnivariateDriftCalculator
 from nannyml.exceptions import InvalidArgumentsException, NotFittedException
 from nannyml.performance_calculation.calculator import PerformanceCalculator
@@ -195,6 +203,24 @@ def sample_regression_realized_perf_result() -> PerformanceCalculationResults:  
     return realized_performance
 
 
+@pytest.fixture(scope="module")
+def sample_missing_value_result() -> MissingValueResults:
+    reference, analysis, _ = load_synthetic_car_loan_data_quality_dataset()
+
+    calc = MissingValuesCalculator(
+        column_names=[
+            'car_value',
+            'salary_range',
+            'debt_to_income_ratio',
+            'loan_length',
+            'repaid_loan_on_prev_car',
+            'size_of_downpayment',
+            'driver_tenure',
+        ],
+    ).fit(reference)
+    return calc.calculate(data=analysis)
+
+
 def test_alert_count_ranking_raises_invalid_arguments_exception_when_drift_result_is_empty(  # noqa: D103
     sample_drift_result,
 ):
@@ -208,7 +234,8 @@ def test_alert_count_ranking_raises_invalid_arguments_exception_when_drift_resul
 def test_alert_count_ranking_raises_invalid_drift_object(sample_realized_perf_result):  # noqa: D103
     ranking = AlertCountRanker()
     with pytest.raises(
-        InvalidArgumentsException, match='Univariate Results object required for drift_calculation_result argument.'
+        InvalidArgumentsException,
+        match='Univariate, Data Quality or Simple Statistics Result class required for drift_calculation_result argument.'
     ):
         ranking.rank(
             sample_realized_perf_result.filter(period='all', metrics=['roc_auc']),
@@ -235,6 +262,12 @@ def test_alert_count_ranking_raises_multiple_continuous_drift_metrics(sample_dri
 def test_alert_count_ranking_contains_rank_column(sample_drift_result):  # noqa: D103
     ranking = AlertCountRanker()
     sut = ranking.rank(sample_drift_result.filter(methods=['jensen_shannon']))
+    assert 'rank' in sut.columns
+
+
+def test_alert_count_ranking_contains_rank_column_on_dq_results(sample_missing_value_result):  # noqa: D103
+    ranking = AlertCountRanker()
+    sut = ranking.rank(sample_missing_value_result)
     assert 'rank' in sut.columns
 
 
@@ -265,6 +298,19 @@ def test_correlation_ranking_contains_rank_column(sample_drift_result, sample_re
     )
     sut = ranking.rank(
         sample_drift_result.filter(period='all', methods=['jensen_shannon']),
+        sample_realized_perf_result.filter(period='all', metrics=['roc_auc']),
+    )
+    assert 'rank' in sut.columns
+
+def test_correlation_ranking_contains_rank_column_on_dq_results(sample_missing_value_result, sample_realized_perf_result):  # noqa: D103
+    ranking = CorrelationRanker()
+    ranking.fit(
+        reference_performance_calculation_result=sample_realized_perf_result.filter(
+            period='reference', metrics=['roc_auc']
+        ),
+    )
+    sut = ranking.rank(
+        sample_missing_value_result,
         sample_realized_perf_result.filter(period='all', metrics=['roc_auc']),
     )
     assert 'rank' in sut.columns
