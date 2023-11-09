Fix threshold computation handling nan values (#333)
* replace std and mean with nanstd and nanmean

* fix test

* Added test to properly check outcome

---------

Co-authored-by: Giovanni Davoli <giovanni.davoli@axyon.ai>
Co-authored-by: Niels Nuyttens <niels@nannyml.com>

tests/test_thresholds.py
@@ -72,7 +72,7 @@ def test_standard_deviation_threshold_init_sets_default_instance_attributes():
 
     assert sut.std_lower_multiplier == 3
     assert sut.std_upper_multiplier == 3
-    assert sut.offset_from == np.mean
+    assert sut.offset_from == np.nanmean
 
 
 @pytest.mark.parametrize(
@@ -153,3 +153,11 @@ def test_standard_deviation_threshold_raises_threshold_exception_when_negative_l
 def test_standard_deviation_threshold_raises_threshold_exception_when_negative_upper_multiplier_given():
     with pytest.raises(ThresholdException, match="'std_upper_multiplier' should be greater than 0 but got value -1"):
         _ = StandardDeviationThreshold(0, -1)
+
+
+def test_standard_deviation_threshold_deals_with_nan_values():
+    t = StandardDeviationThreshold()
+    upper, lower = t.thresholds(np.asarray([-1, 1, np.nan, 1, np.nan]))
+
+    assert not np.isnan(upper)
+    assert not np.isnan(lower)