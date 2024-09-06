cfc58985b997614f26379942ec1864f34cf8882a
Niels Nuyttens <niels@nannyml.com>
2024-09-06 16:08:03 +0200

    [skip ci] fix incorrect version

--- nannyml/__init__.py
+++ nannyml/__init__.py
@@ -31,7 +31,7 @@ __email__ = 'niels@nannyml.com'
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
-__version__ = '0.12.0'
+__version__ = '0.11.0'
 
 import logging
 
